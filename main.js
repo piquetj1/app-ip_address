7c0c908b6db43a68444c619f66831266df1e98d9
piquetj1 <john.piquettejr@centurylink.com>
2020-05-06 21:41:20 +0000

    Lab 4

--- main.js
+++ main.js
@@ -48,7 +48,9 @@ class IpAddress {
 getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
-  let firstIpAddress = null;
+   let firstIpAddress = null;
+  let ipv4 = null;
+  let ipv6 = null;
   let callbackError = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
@@ -68,7 +70,9 @@ getFirstIpAddress(cidrStr, callback) {
   } else {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
-    [firstIpAddress] = cidr.toArray(options);
+     let ipv4 = cidr.toArray(options)[0];
+    let ipv6 = getIpv4MappedIpv6Address(ipv4);
+    firstIpAddress = { ipv4, ipv6 };
   }
 
   
